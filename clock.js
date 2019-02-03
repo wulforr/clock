f2d254abb2c22637733381c4c5df337fa655072d
Shaurya Vardhan Singh <wulforr@github.com>
2019-02-03 12:45:38 +0530

    tweaked hour hand

--- clock.js
+++ clock.js
@@ -5,15 +5,24 @@ function setDate(){
     var now = new Date();
     var seconds = now.getSeconds();
     var secondDegrees = ((seconds/60)*360)+90;
+    console.log(secondDegrees);
     secondHand.style.transform = "rotate(" + secondDegrees + "deg)" ;
     
     var minutes = now.getMinutes();
     var minuteDegrees = ((minutes/60)*360)+90;
     minuteHand.style.transform = "rotate(" + minuteDegrees + "deg)" ;
+    var hourDegrees = ((minutes/60)*30);
 
     var hours = now.getHours();
-    var hourDegrees = ((hours/12)*360)+90;
+    hourDegrees += ((hours/12)*360)+90;
     hourHand.style.transform = "rotate(" + hourDegrees + "deg)" ;
-    console.log(hours);
+    if(secondDegrees==90)
+    {
+        var hand = document.querySelectorAll(".hand");
+        for(var i=0;i<hand.length;i++)
+        {
+            hand[i].style.transition = "all 0s";
+        }
+    }
 }
 setInterval(setDate, 1000);
